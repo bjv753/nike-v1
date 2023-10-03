4ca89d1f70ae975a99783d8f71f67fc992a984c0
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-03 13:54:53 -0500

    update

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+
+export  {Footer, Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe} 
